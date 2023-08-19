8fabec8e7e966e90b0c1520b9c43e3dd505052fe
Jaesuk <jaesuk95@outlook.com>
2023-08-19 14:24:30 +0900

    Dice game from inflearn

--- DiceGame/ViewController.swift
+++ DiceGame/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Dice images
+    @IBOutlet weak var firstImage: UIImageView!
+    @IBOutlet weak var secondImage: UIImageView!
+    
+    var diceArray: [UIImage] = [#imageLiteral(resourceName: "black1"), #imageLiteral(resourceName: "black2"), #imageLiteral(resourceName: "black3"), #imageLiteral(resourceName: "black4"), #imageLiteral(resourceName: "black5"), #imageLiteral(resourceName: "black6")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        // 첫번째 이미지 뷰의 이미지를 랜덤으로 변경
+        // 두번째 이미지 뷰의 이미지를 랜덤으로 변경
+        
+        firstImage.image = diceArray.randomElement()
+        secondImage.image = diceArray.randomElement()
+    }
 
 }
 
